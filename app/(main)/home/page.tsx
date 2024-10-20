b9f0ead29b369f4782ba77782e64cb96bfe887e0
M21 <wyattmohammad1371017@gmail.com>
2024-10-21 01:19:14 +0330

    fix errors

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -1,14 +1,15 @@
-import { Metadata } from "next";
-import React from "react";
-import TwittsWrapper from "@/app/_ui/TwittsWrapper";
-import Header from "@/app/_ui/Header";
-import { User, UserWithFollows } from "@/app/_lib/definitions";
 import { auth } from "@/app/_lib/auth";
+import { UserWithFollows } from "@/app/_lib/definitions";
+import Header from "@/app/_ui/Header";
+import TwittsWrapper from "@/app/_ui/TwittsWrapper";
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Home"
 }
+
+export const dynamic = 'force-dynamic';
 
 async function Page() {
   const session = await auth();

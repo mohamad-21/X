b6277f3a49256e70f5f6efca4b942c977abffc3b
M21 <wyattmohammad1371017@gmail.com>
2024-10-22 00:27:58 +0330

    fix errors

---
+++ app/api/user/details/route.ts
@@ -0,0 +1,28 @@
+import { getUserBookmarks, getUserById, getUserFollowersAndFollowings } from "@/app/_lib/actions";
+import { User, UserData } from "@/app/_lib/definitions";
+import { NextRequest, NextResponse } from "next/server";
+
+export const dynamic = 'force-dynamic';
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = request.nextUrl.searchParams.get('id');
+    const twittsType = request.nextUrl.searchParams.get('twitts_type') || '';
+    if (!userId) return NextResponse.json({ message: 'id parameter is not set' }, { status: 400 });
+    const user = await getUserById(userId, { twittsWithReply: twittsType === 'with_reply', mediaOnly: twittsType === 'media_only' });
+    if (!user) {
+      return NextResponse.json({
+        message: 'user not found'
+      }, { status: 404 });
+    }
+    const [follows, bookmarks] = await Promise.all([
+      getUserFollowersAndFollowings(user.id),
+      getUserBookmarks(user.id)
+    ]);
+    return NextResponse.json({ ...user, follows, bookmarks });
+  } catch (err) {
+    return NextResponse.json({
+      message: 'internal server error'
+    }, { status: 500 });
+  }
+}
